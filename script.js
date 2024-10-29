78c14fcd0a41cafb7abd346325dbdea7d293420c
Shakarof_bp216 <yalcinys-bp216@code.edu.az>
2024-10-29 05:32:30 +0400

    organize

--- script.js
+++ script.js
@@ -1,24 +1,63 @@
-function makeNumber() {
-    return Math.floor(Math.random() * 20);
-}
-let  computerNumber = makeNumber()
-console.log(computerNumber);
 
-const your_number= document.querySelector(".your_number")
-const check_button= document.querySelector(".check_button")
+const score_contanier = document.querySelector(".score_contanier");
+const your_number = document.querySelector(".your_number");
+const check_button = document.querySelector(".check_button");
+const again_button = document.querySelector(".again_button");
+const score_part_right_text_top=document.querySelector(".score_part_right_text_top");
+const Game_page = document.querySelector(".Game_page");
+
+
+
+
+
+
+function makeNumber() {
+  return Math.floor(Math.random() * 3); 
+}
+
+function displayMessage(text) {
+  score_part_right_text_top.textContent = text;
+}
+
+let computerNumber = makeNumber();
+let score = 20;
+
+
 
 check_button.addEventListener("click", function () {
-    let mynumber=your_number.value
+  let mynumber = parseInt(your_number.value);
+
+  if (!mynumber) {
+    displayMessage("Rəqəm yazın!");
+
    
+  } else if (mynumber === computerNumber) {
+    displayMessage("Təbriklər, doğru təxmin!");
+
+    Game_page.style.backgroundColor = "red";
+
+   
+  } else if (mynumber > computerNumber) {
+    displayMessage("Aşağı!");
+    score--;
+
+
+  } else if (mynumber < computerNumber) {
+    displayMessage("Yuxarı!");
+    score--;
+  }
+
+  
+  if (score === 0) {
+    displayMessage("Oyunu uduzdunuz!");
+  }
 });
-let mynumber=your_number.value
-if (mynumber==computerNumber){
-  console.log("salam");
-}
 
 
- 
-
-
-
-
+again_button.addEventListener("click", function () {
+  score = 20;
+  computerNumber = makeNumber(); 
+  displayMessage("Start guessing...");
+  document.body.style.backgroundColor = "black";
+  your_number.value = "";
+});
